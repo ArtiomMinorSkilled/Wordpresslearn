17e0372f9bfe1f916dfc4ef609a792a898c179cf
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2022-02-25 17:21:10 +0100

    Disable widget

--- index.php
+++ index.php
@@ -8,11 +8,6 @@
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since Twenty Nineteen 1.0
  */
 
 get_header();
